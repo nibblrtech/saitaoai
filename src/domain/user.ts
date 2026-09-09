964bee930168e39eaa5bfd5beefbbf5432abd58f
nibblrtech <nibblrtech@gmail.com>
2026-09-09 14:49:39 -0400

    removed prefix

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -7,5 +7,5 @@ export interface User {
 }
 
 export function createUser(id: string, email: string): User {
-  return { id: "prefix" + id, email};//, userRepository: new UserRepository() };
+  return { id: id, email};//, userRepository: new UserRepository() };
 }
