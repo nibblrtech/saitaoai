5bec4408a79b47572216dd8b986a4c2ce0f7002b
nibblrtech <nibblrtech@gmail.com>
2026-09-10 06:36:57 -0400

    fix constraint

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -7,5 +7,5 @@ export interface User {
 }
 
 export function createUser(id: string, email: string): User {
-  return { id: "ddd"+id, email};//, userRepository: new UserRepository() };
+  return { id: id, email};//, userRepository: new UserRepository() };
 }
