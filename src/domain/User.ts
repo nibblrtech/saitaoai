341c8f5b05ad299164379e6097bcbab3054e69be
nibblrtech <nibblrtech@gmail.com>
2026-09-10 16:00:00 -0400

    changed a new violation for an old

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -1,13 +1,13 @@
-//import { UserRepository } from "../infrastructure/userRepository";
+import { UserRepository } from "../infrastructure/userRepository";
 
 export interface User {
   id: string;
   email: string;
   createdAt: Date;
-  updated_at: Date;
-  //userRepository: UserRepository;
+  updatedAt: Date;
+  userRepository: UserRepository;
 }
 
 export function createUser(id: string, email: string): User {
-  return { id: id, email, createdAt: new Date(), updated_at: new Date() };//, userRepository: new UserRepository() };
+  return { id: id, email, createdAt: new Date(), updatedAt: new Date(), userRepository: new UserRepository() };
 }
