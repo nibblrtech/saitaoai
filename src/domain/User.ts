791f8586a6601ca9deeb51bd5354717f8aaf1785
nibblrtech <nibblrtech@gmail.com>
2026-09-10 16:03:59 -0400

    went back to the updated at problem but did it without stupid mistake

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -1,12 +1,9 @@
-//import { UserRepository } from "../infrastructure/userRepository";
-
 export interface User {
   id: string;
   email: string;
   createdAt: Date;
-//  userRepository: UserRepository;
 }
 
 export function createUser(id: string, email: string): User {
-  return { id: id, email, createdAt: new Date(), updatedAt: new Date()};//, userRepository: new UserRepository() };
+  return { id: id, email, createdAt: new Date() };
 }
